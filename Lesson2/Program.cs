ac4136e38ee759e45697c3da0da4e70fefccbd99
FL <l.u.1980@mail.ru>
2023-12-17 23:24:48 +0300

    Initial commit

---
+++ Lesson2/Program.cs
@@ -0,0 +1,163 @@
+﻿////////////////////////////
+// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 ->  нет
+// 161 -> да
+
+int number = new Random().Next(1, 1000);  
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int divOne = number % 7; 
+int divTwo = number % 23;  
+if (divOne == 0 && divTwo == 0)
+{
+    System.Console.WriteLine($"{number} -> да");
+}
+else
+{
+    System.Console.WriteLine($"{number} -> нет (остатки от деления {number} % 7 - {divOne} и {number} % 23 - {divTwo})");
+}
+System.Console.WriteLine();
+
+//++++++++++++++++++++++++++++++++
+
+///////// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+int x;
+int y;
+
+System.Console.Write("Введите координату X, не равную нулю: ");
+x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите координату Y, не равную нулю: ");
+y = Convert.ToInt32(Console.ReadLine());
+if (x == 0 || y == 0)
+System.Console.WriteLine("Ни одна из координат не должа быть равна 0");
+if (x > 0 && y > 0)
+System.Console.WriteLine("Точка находится в 1-й четверти");
+if (x < 0 && y > 0)
+System.Console.WriteLine("Точка находится во 2-й четверти");
+if (x < 0 && y < 0)
+System.Console.WriteLine("Точка находится в 3-й четверти");
+if (x > 0 && y < 0)
+System.Console.WriteLine("Точка находится в 4-й четверти");
+
+//++++++++++++++++++++++++++++++++
+
+// // Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+int number = new Random().Next(10, 100); // Правая граница должна быть на 1 больше диапазона. 
+System.Console.WriteLine(number);
+int first = number / 10;
+int second = number % 10;
+if (first > second)
+{
+    System.Console.WriteLine($"Первое число {first} больше второго {second}");
+}
+else if (first < second)
+{
+    System.Console.WriteLine($"Второе число {second} больше первого {first}");
+}
+else 
+{
+System.Console.WriteLine($"Второе число {second} равно первому {first}");
+}
+
+//++++++++++++++++++++++++++++++++
+
+// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+int number = new Random().Next(1, 121); // Правая граница должна быть на 1 больше диапазона. 
+System.Console.WriteLine(number);
+if (number >= 1 && number < 10)
+System.Console.WriteLine($"Однозначное число: {number}");
+if (number >= 10 && number < 100)
+System.Console.WriteLine($"Двузначное число: {number/10}, {number%10}");
+if (number >= 100 && number < 1000)
+System.Console.WriteLine($"Трехзначное число: {number/100}, {number%100/10}, {number%10}");
+
+
+// Пример вы найдете в конце презентации к семинару.
+
+//++++++++++++++++++++++++++++++++
+
+
+
+///////////////////////////////////
+// Принимает 2 числа и проверяет является ли одно из них квадратом другого.
+// 25, 5 ->  да
+// 8, 9 -> нет
+// System.Console.Write("Введите первое число: ");
+// int numberOne = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите второе число: ");
+// int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+// int resultOne = numberOne * numberOne; 
+// int resultTwo = numberTwo * numberTwo;  
+
+// if (resultOne == numberTwo || resultTwo == numberOne)
+// {
+//     System.Console.WriteLine($"{numberOne}, {numberTwo} -> да");
+// }
+// else
+// {
+//     System.Console.WriteLine($"{numberOne}, {numberTwo} -> нет");
+// }
+// System.Console.WriteLine();
+
+
+//////////////////////
+// Получает 2 числа, определяет кратно ли второе число первому. Если не кратно, выводит остаток от деления.
+// 16, 4 -> кратно
+// 34, 5 -> не кратно, остаток 4
+// int numberOne = new Random().Next(1, 100);  
+// int numberTwo = new Random().Next(1, 10);  
+
+// int result = numberOne % numberTwo;
+// int division = numberOne / numberTwo;
+// if (result == 0)
+// {
+//     System.Console.WriteLine($"{numberOne}, {numberTwo} -> кратно");
+// }
+// else
+// {
+//     System.Console.WriteLine($"{numberOne}, {numberTwo} -> не кратно, остаток {result} ({numberOne} / {numberTwo} = {division})");
+// }
+// System.Console.WriteLine();
+
+
+
+
+
+////////////////////////
+// Первоеолучает трехзначное число, удаляет вторую цифру, выводит двухзначное число.
+// int number = new Random().Next(1000, 10000); // Правая граница должна быть на 1 больше диапазона. 
+// int leftDigit = number / 100;
+// int rightDigit = number % 10;
+// if (number > 99 && number <1000)
+// {
+//     System.Console.WriteLine($"{number} -> {leftDigit}{rightDigit}");
+// }
+// else
+// {
+//         System.Console.WriteLine($"{number} -> Не трёхзначное число");
+// }
+// System.Console.WriteLine();
+
+
+
+
+//////////////////////////
+// Вывести случайное число от 10 до 99. Определить большее из них.
+// int number = new Random().Next(10, 100); // Правая граница должна быть на 1 больше диапазона. 
+// int leftDigit = number / 10;
+// int rightDigit = number % 10;
+// if (leftDigit > rightDigit)
+// {
+//     System.Console.WriteLine($"{number} -> {leftDigit}");
+// }
+// else
+// {
+//         System.Console.WriteLine($"{number} -> {rightDigit}");
+// }
+// System.Console.WriteLine();
+
---
+++ Lesson4/Program.cs
@@ -0,0 +1,84 @@
+﻿// Урок 4. Функции
+// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+
+string ReadUserInput()
+{
+    while (true)
+    {
+    System.Console.Write("Введите целое число (завершит программу ввод q или если сумма всех цифр числа четная): ");
+    string userInput = System.Console.ReadLine()!;
+    if (userInput == "q") return userInput;
+    int countEven = 0;
+    foreach (char symbol in userInput)
+    {
+        countEven = countEven + Convert.ToInt32(symbol);
+    }
+    if (countEven % 2 == 0) return userInput;
+    }
+}
+
+ReadUserInput();
+
+// Задача 2: Задайте массив заполненный случайными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CountEvenNumbers(int[] array)
+{
+int countEven =0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) countEven++;
+    }
+    System.Console.WriteLine($"Количество четных чисел в массиве: {countEven}");
+    return array;
+}
+
+int ReadInt (string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int[] GenerateArray(int size, int leftRange, int rightRange) 
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+        for (int j = 0; j < size; j++)
+        {
+            tempArray[j] = rand.Next(leftRange, rightRange+1);
+        }
+        return tempArray;
+}
+
+int size = ReadInt("Введите размер массива: ");
+int[] myArray = GenerateArray(size, 100, 1000);
+PrintArray(myArray);
+CountEvenNumbers(myArray); // для следующей задачи эта строка сделана комментарием, для задачи 2 ее комментарий нужно убрать
+
+
+
+// Задача 3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+int size2 = ReadInt("Введите размер массива: ");
+int[] myArray2 = GenerateArray(size2, 1, 50);
+PrintArray(myArray2);
+int last = size2 - 1;
+int temp = 0;
+for (int i = 0; i < myArray2.Length / 2; i++)
+{
+    temp = myArray2[i];
+    myArray2[i] = myArray2[last];
+    myArray2[last] = temp;
+    last--;
+}
+PrintArray(myArray2);
+
+// ***********************************************************
+
+
